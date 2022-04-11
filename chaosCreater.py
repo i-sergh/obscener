5eacb0b4d6f0bc1844bfda9452c0f11b4a034ca9
Sergey Ippolitov <seripp28@gmail.com>
2022-04-11 18:24:59 +0300

    fully create chaosCreator - test function

--- chaosCreater.py
+++ chaosCreater.py
@@ -1,17 +1,62 @@
 import re
 import os
 
-#print(os.listdir())
 
-def creater():
-    os.makedirs('test\empty', exist_ok=True)
-    os.makedirs('test\empty2\eeh', exist_ok=True)
+"""
+creates:
+    test
+        |_ test
+        |        |_empty
+        |        |_empty2
+        |        |              |_eeh
+        |        |_nonempty
+        |        |                  |_empty
+        |        |                  |            |_empty
+        |        |                  |_lolkek.txt
+        |        |                  |_NOTket.txt
+        |        |_notempty
+        |                        |_owch
+        |                        |_nonempty2
+        |                        |                   |_empty2
+        |                        |_ kekt.txt  
+        |_kek1.txt                                           
+        |_kek.txt
+        |_lolkek1.txt
+                              
+
+"""
+
+
+def folderCreater():
+    os.makedirs('test\\empty', exist_ok=True)
+    os.makedirs('test\\empty2\\eeh', exist_ok=True)
+    os.makedirs('test\\nonempty\\empty\\empty', exist_ok=True)
+    os.makedirs('test\\notempty\\owch', exist_ok=True)
+    
+    os.makedirs('test\\notempty\\nonempty2\\empty2', exist_ok=True)
 
 if not 'test' in os.listdir():
     os.mkdir('test')
-else:
-    os.chdir('test')
     
-    creater()
-    print(os.listdir())
+os.chdir('test')
+    
+folderCreater()
+f = open("kek1.txt", "w+")
+f.close()
+f = open("kek.txt", "w+")
+f.close()
+f = open("lolkek1.txt", "w+")
+f.close()
+
+os.chdir('test\\nonempty')
+f = open("lolkek.txt", "w+")
+f.close()
+f = open("NOTket.txt", "w+")
+f.close()
+os.chdir('..')
+os.chdir('notempty\\nonempty2')
+f = open("kekt.txt", "w+")
+f.close()
+os.chdir('..\\..\\..')
+print(os.listdir())
     
